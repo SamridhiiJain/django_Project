fef21021ea3275791c7caeac6e532685e010fb54
SamridhiiJain <samridhijain2608@gmail.com>
2024-07-14 17:28:59 +0530

    daily updates

--- main/views.py
+++ main/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request ,'index.html')
+
+def login_view(request):
+    if request.method=='POST':
+        username= request.POST.get('username')
+        password= request.POST.get('password')
+        print(username, password)
+    return render(request,'login.html')
+def register_view(request):
+    if request.method=='POST':
+        username= request.POST.get('username')
+        email= request.POST.get('email')
+        password= request.POST.get('pwd')
+        cpassword= request.POST.get('cpwd')
+        print(username,email,password,cpassword)
+    return render(request,'register.html')
+
